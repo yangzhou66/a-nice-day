3499125386ac6bf55e356c1193c829732c27dfea
yangzhou66 <1074355570@qq.com>
2018-04-12 23:50:50 +0800

    啊啊啊

--- firstDemoRedux/src/components/App.js
+++ firstDemoRedux/src/components/App.js
@@ -6,12 +6,14 @@ import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
-const App = () => (
+const App = () => {
+    console.log(this)
+    return (
     <div>
         <AddTodo />
         <VisibleTodoList />
         <Footer />
     </div>
-)
+)}
 
 export default App
--- firstDemoRedux/webpack.config.js
+++ firstDemoRedux/webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
 module.exports = {
     //entry: './src/index.js',
+
     entry: {
         app: './src/index.js',
         //print: './src/print.js'
@@ -15,22 +17,45 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },
-    devtool:'inline-source-map',
-    devServer:{
-        contentBase:'./dist'
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     module: {
         rules: [
             {test: /\.css$/, use: ['style-loader', 'css-loader']},
             {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
             {test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        //dont compress html code
+                        minimize: false
+                    }
+                }]
+            },
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname,'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        compact: false,
+                        presets: ['react-app']
+                    }
+                }
+            },
 
         ]
     },
-       plugins: [
-           new CleanWebpackPlugin(['dist']),
-         new HtmlWebpackPlugin({
-               title: 'Output Management'
-     })
-  ],
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: path.resolve(path.resolve(__dirname, 'src'), 'index.html'),
+        })
+    ],
 };
